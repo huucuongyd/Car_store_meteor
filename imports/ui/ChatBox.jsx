da36df5ab30e8a51b01fc393efdc824a3941276b
Cuong <huucuongyd@gmail.com>
2023-06-27 09:27:37 +0700

    fix poition message and autoload message

--- imports/ui/ChatBox.jsx
+++ imports/ui/ChatBox.jsx
@@ -1,28 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Messages } from '../api/Messages';
+
+
 
 export const ChatForm = ({ user }) => {
   const [newMessage, setNewMessage] = useState('');
   const [messChange, setMessChange] = useState([]);
 
+  const chatboxRef = useRef(null);
+
   useEffect(() => {
     const handleMessagesChange = () => {
       const messages = Messages.find().fetch();
       setMessChange(messages);
+      scrollToBottom();
     };
 
     const subscription = Meteor.subscribe('messages');
-    const observer = Messages.find().observeChanges({
-      added: handleMessagesChange,
-      changed: handleMessagesChange,
-      removed: handleMessagesChange,
+    const computation = Tracker.autorun(() => {
+      Messages.find().fetch();
+      handleMessagesChange();
     });
 
     return () => {
       subscription.stop();
-      observer.stop();
+      computation.stop();
     };
   }, []);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messChange]);
 
   const handleMessageChange = (event) => {
     setNewMessage(event.target.value);
@@ -36,16 +44,36 @@ export const ChatForm = ({ user }) => {
     }
   };
 
+  const scrollToBottom = () => {
+
+    if (chatboxRef.current) {
+        chatboxRef.current.scrollTop = chatboxRef.current.scrollHeight;
+    }
+  };
+
   return (
-    <div className="chatbox">
+    <div className="chatbox" >
       <div className="chatbox-header">Chatbox</div>
-      <div className="chatbox-messages">
-        {messChange.map((message) => (
-          <div key={message._id} className="message">
-            <div className="sender">{message.u.username}</div>
-            <div className="text">{message.text}</div>
-          </div>
-        ))}
+      <div className="chatbox-messages" ref={chatboxRef}>
+        {messChange.map((message) =>{
+          if(message.u.username == user.username){
+
+            return (
+              <div key={message._id} className="message">
+                <div className="sender-right">{message.u.username}</div>
+                <div className="text-right">{message.text}</div>
+              </div>
+            )
+          }else{
+            return (
+              <div key={message._id} className="message">
+                <div className="sender">{message.u.username}</div>
+                <div className="text">{message.text}</div>
+              </div>
+            )
+          }
+
+        })}
       </div>
       <form className="chatbox-input" onSubmit={handleSendClick}>
         <input
